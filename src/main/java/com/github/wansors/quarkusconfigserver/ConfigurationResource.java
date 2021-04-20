58ab2d9eba1c60ab6836f9f5efa79a152a25dd31
wansors <wansors@gmail.com>
2021-04-20 15:24:03 +0200

    Added initial structure

---
+++ src/main/java/com/github/wansors/quarkusconfigserver/ConfigurationResource.java
@@ -0,0 +1,37 @@
+package com.github.wansors.quarkusconfigserver;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.jboss.logging.Logger;
+
+
+@Path("/config")
+public class ConfigurationResource {
+
+    private static final Logger LOG = Logger.getLogger(ConfigurationResource.class);
+
+    /**
+     * 
+     * /{application}/{profile}[/{label}]
+/{application}-{profile}.yml
+/{application}-{profile}.properties
+/{application}-{profile}.json
+/{label}/{application}-{profile}.yml
+/{label}/{application}-{profile}.properties
+
+     * 
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{label}/{application}-{profile}.json")
+    public Response standardLabelApplicationProfileJson(@PathParam("label") String label,@PathParam("application") String application,@PathParam("profile") String profile) {
+        LOG.info("Obtaining config for app: "+application+" profile: "+profile+" label: "+label);
+        return Response.ok().build();
+    }
+
+}
