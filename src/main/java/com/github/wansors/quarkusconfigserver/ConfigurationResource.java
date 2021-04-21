b8153ca8db959b77d529d1df450431ea8708d3b9
Joan <jgspascual@gmail.com>
2021-04-21 15:15:48 +0200

    Added first draft

--- src/main/java/com/github/wansors/quarkusconfigserver/ConfigurationResource.java
+++ src/main/java/com/github/wansors/quarkusconfigserver/ConfigurationResource.java
@@ -1,5 +1,7 @@
 package com.github.wansors.quarkusconfigserver;
 
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -9,29 +11,56 @@ import javax.ws.rs.core.Response;
 
 import org.jboss.logging.Logger;
 
-
 @Path("/config")
+@RequestScoped
 public class ConfigurationResource {
 
     private static final Logger LOG = Logger.getLogger(ConfigurationResource.class);
 
+    @Inject
+    ConfigurationService service;
+
+    private String configuration;
+
     /**
      * 
      * /{application}/{profile}[/{label}]
-/{application}-{profile}.yml
-/{application}-{profile}.properties
-/{application}-{profile}.json
-/{label}/{application}-{profile}.yml
-/{label}/{application}-{profile}.properties
-
      * 
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/{label}/{application}-{profile}.json")
-    public Response standardLabelApplicationProfileJson(@PathParam("label") String label,@PathParam("application") String application,@PathParam("profile") String profile) {
-        LOG.info("Obtaining config for app: "+application+" profile: "+profile+" label: "+label);
-        return Response.ok().build();
+    public Response standardLabelApplicationProfileJson(@PathParam("label") String label,
+            @PathParam("application") String application, @PathParam("profile") String profile) {
+        LOG.info("Obtaining config for app: " + application + " profile: " + profile + " label: " + label + " on JSON");
+        configuration = service.getConfiguration(application, profile, label);
+        return Response.ok(configuration).build();
     }
 
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{application}-{profile}.json")
+    public Response standardApplicationProfileJson(@PathParam("application") String application,
+            @PathParam("profile") String profile) {
+        return standardLabelApplicationProfileJson(null, application, profile);
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/{label}/{application}-{profile}.properties")
+    public Response standardLabelApplicationProfileProperties(@PathParam("label") String label,
+            @PathParam("application") String application, @PathParam("profile") String profile) {
+        LOG.info("Obtaining config for app: " + application + " profile: " + profile + " label: " + label
+                + " on PROPERTIES");
+        configuration = service.getConfiguration(application, profile, label);
+        return Response.ok(configuration).build();
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/{application}-{profile}.properties")
+    public Response standardApplicationProfileProperties(@PathParam("application") String application,
+            @PathParam("profile") String profile) {
+        return standardLabelApplicationProfileProperties(null, application, profile);
+    }
 }
---
+++ src/main/java/com/github/wansors/quarkusconfigserver/ConfigurationService.java
@@ -0,0 +1,9 @@
+package com.github.wansors.quarkusconfigserver;
+
+public class ConfigurationService {
+
+    public String getConfiguration(String application, String profile, String label) {
+        return null;
+    }
+
+}
