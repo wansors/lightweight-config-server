8e9ec1e6cc47a82900c1151a9f0893d6978f8e37
wansors <wansors@gmail.com>
2021-05-19 23:17:12 +0200

    Added logic to map types on yaml loading

--- src/main/java/com/github/wansors/quarkusconfigserver/SpringYamlConfigSource.java
+++ src/main/java/com/github/wansors/quarkusconfigserver/SpringYamlConfigSource.java
@@ -1,13 +1,10 @@
 package com.github.wansors.quarkusconfigserver;
 
-import static java.util.Collections.singletonMap;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UncheckedIOException;
 import java.net.URL;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -30,132 +27,136 @@ import io.smallrye.config.common.MapBackedConfigSource;
  * @author <a href="mailto:david.lloyd@redhat.com">David M. Lloyd</a>
  */
 public class SpringYamlConfigSource extends MapBackedConfigSource {
-    private static final long serialVersionUID = -418186029484953531L;
+	private static final long serialVersionUID = -418186029484953531L;
 
-    private static final String NAME_PREFIX = "SpringYamlConfigSource[source=";
+	private static final String NAME_PREFIX = "SpringYamlConfigSource[source=";
+	public static final String TYPE_PREFIX = "SpringYamlConfigSource.type.";
 
-    private final Set<String> propertyNames;
+	private final Set<String> propertyNames;
 
-    public SpringYamlConfigSource(String name, Map<String, String> source, int ordinal) {
-        super(name, source, ordinal, false);
-        this.propertyNames = filterPropertyNames(source);
-    }
+	public SpringYamlConfigSource(String name, Map<String, String> source, int ordinal) {
+		super(name, source, ordinal, false);
+		propertyNames = filterPropertyNames(source);
+	}
 
- 
+	public SpringYamlConfigSource(URL url, int ordinal) throws IOException {
+		this(NAME_PREFIX + url.toString() + "]",
+				ClassPathUtils.readStream(url, (Function<InputStream, Map<String, String>>) inputStream -> {
+					try {
+						return streamToMap(inputStream);
+					} catch (IOException e) {
+						throw new UncheckedIOException(e);
+					}
+				}), ordinal);
+	}
 
-    public SpringYamlConfigSource(URL url, int ordinal) throws IOException {
-        this(NAME_PREFIX + url.toString() + "]",
-                ClassPathUtils.readStream(url, (Function<InputStream, Map<String, String>>) inputStream -> {
-                    try {
-                        return streamToMap(inputStream);
-                    } catch (IOException e) {
-                        throw new UncheckedIOException(e);
-                    }
-                }), ordinal);
-    }
+	@Override
+	public Set<String> getPropertyNames() {
+		return propertyNames;
+	}
 
+	@SuppressWarnings("unchecked")
+	private static Map<String, String> streamToMap(InputStream inputStream) throws IOException {
+		Assert.checkNotNullParam("inputStream", inputStream);
+		final Map<String, String> yamlInput = new TreeMap<>();
+		try {
+			final Iterable<Object> objects = new Yaml(new StringConstructor()).loadAll(inputStream);
+			for (Object object : objects) {
+				if (object instanceof Map) {
+					yamlInput.putAll(yamlInputToMap((Map<Object, Object>) object));
+				}
+			}
+			inputStream.close();
+		} catch (Throwable t) {
+			try {
+				inputStream.close();
+			} catch (Throwable t2) {
+				t.addSuppressed(t2);
+			}
+			throw t;
+		}
+		return yamlInput;
+	}
 
-    @Override
-    public Set<String> getPropertyNames() {
-        return propertyNames;
-    }
+	private static Map<String, String> yamlInputToMap(final Map<Object, Object> yamlInput) {
+		final Map<String, String> properties = new TreeMap<>();
+		if (yamlInput != null) {
+			flattenYaml("", yamlInput, properties, false);
+		}
+		return properties;
+	}
 
-    @SuppressWarnings("unchecked")
-    private static Map<String, String> streamToMap(InputStream inputStream) throws IOException {
-        Assert.checkNotNullParam("inputStream", inputStream);
-        final Map<String, String> yamlInput = new TreeMap<>();
-        try {
-            final Iterable<Object> objects = new Yaml(new StringConstructor()).loadAll(inputStream);
-            for (Object object : objects) {
-                if (object instanceof Map) {
-                    yamlInput.putAll(yamlInputToMap((Map<Object, Object>) object));
-                }
-            }
-            inputStream.close();
-        } catch (Throwable t) {
-            try {
-                inputStream.close();
-            } catch (Throwable t2) {
-                t.addSuppressed(t2);
-            }
-            throw t;
-        }
-        return yamlInput;
-    }
+	@SuppressWarnings("unchecked")
+	private static void flattenYaml(String path, Map<Object, Object> source, Map<String, String> target,
+			boolean indexed) {
+		source.forEach((originalKey, value) -> {
+			String key;
+			if (originalKey == null) {
+				key = "";
+			} else {
+				key = originalKey.toString();
+			}
 
-    @SuppressWarnings("unchecked")
-    private static Map<String, String> stringToMap(String str) {
-        final Map<Object, Object> yamlInput = new Yaml(new StringConstructor()).loadAs(str, HashMap.class);
-        return yamlInputToMap(yamlInput);
-    }
+			if (!key.isEmpty() && path != null && !path.isEmpty()) {
+				key = indexed ? path + key : path + "." + key;
+			} else if (path != null && !path.isEmpty()) {
+				key = path;
+			}
 
-    private static Map<String, String> yamlInputToMap(final Map<Object, Object> yamlInput) {
-        final Map<String, String> properties = new TreeMap<>();
-        if (yamlInput != null) {
-            flattenYaml("", yamlInput, properties, false);
-        }
-        return properties;
-    }
+			if (value == null) {
+				target.put(key, "");
+				target.put(TYPE_PREFIX + key, String.class.getName());
+			} else if (value instanceof String) {
+				target.put(key, (String) value);
+				target.put(TYPE_PREFIX + key, String.class.getName());
+			} else if (value instanceof Map) {
+				flattenYaml(key, (Map<Object, Object>) value, target, false);
+			} else if (value instanceof List) {
+				final List<Object> list = (List<Object>) value;
+				// flattenList(key, list, target);
+				for (int i = 0; i < list.size(); i++) {
+					flattenYaml(key, Collections.singletonMap("[" + i + "]", list.get(i)), target, true);
+				}
+				// Empty List case
+				if (list.isEmpty()) {
+					target.put(key, "[]");
+					target.put(TYPE_PREFIX + key, List.class.getName());
+				}
+			} else {
+				target.put(key, value.toString());
+				target.put(TYPE_PREFIX + key, value.getClass().getName());
 
-    @SuppressWarnings("unchecked")
-    private static void flattenYaml(String path, Map<Object, Object> source, Map<String, String> target, boolean indexed) {
-        source.forEach((originalKey, value) -> {
-            String key;
-            if (originalKey == null) {
-                key = "";
-            } else {
-                key = originalKey.toString();
-            }
+			}
 
-            if (!key.isEmpty() && path != null && !path.isEmpty()) {
-                key = indexed ? path + key : path + "." + key;
-            } else if (path != null && !path.isEmpty()) {
-                key = path;
-            }
+		});
+	}
 
-            if (value == null) {
-                target.put(key, "");
-            } else if (value instanceof String) {
-                target.put(key, (String) value);
-            } else if (value instanceof Map) {
-                flattenYaml(key, (Map<Object, Object>) value, target, false);
-            } else if (value instanceof List) {
-                final List<Object> list = (List<Object>) value;
-                //flattenList(key, list, target);
-                for (int i = 0; i < list.size(); i++) {
-                    flattenYaml(key, Collections.singletonMap("[" + i + "]", list.get(i)), target, true);
-                }
-                //Empty List case
-                if(list.isEmpty()){
-                    target.put(key, "[]");    
-                }
-            } else {
-                target.put(key, (value != null ? value.toString() : ""));
-            }
-        });
-    }   
+	private static Set<String> filterPropertyNames(Map<String, String> source) {
+		final Set<String> filteredKeys = new HashSet<>();
+		for (final String key : new HashSet<>(source.keySet())) {
+			if (key.startsWith(SpringYamlConfigSource.class.getName() + ".filter.")) {
+				String originalKey = key.substring(55);
+				source.put(originalKey, source.remove(key));
+			} else {
+				filteredKeys.add(key);
+			}
+		}
+		return filteredKeys;
+	}
 
-    private static Set<String> filterPropertyNames(Map<String, String> source) {
-        final Set<String> filteredKeys = new HashSet<>();
-        for (final String key : new HashSet<>(source.keySet())) {
-            if (key.startsWith(SpringYamlConfigSource.class.getName() + ".filter.")) {
-                String originalKey = key.substring(55);
-                source.put(originalKey, source.remove(key));
-            } else {
-                filteredKeys.add(key);
-            }
-        }
-        return filteredKeys;
-    }
+	/**
+	 * Override some of the yaml constructors, so that the value written in the
+	 * flatten result is more alike with the source. For instance, timestamps may be
+	 * written in a completely different format which prevents converters to convert
+	 * the correct value.
+	 */
+	private static class StringConstructor extends Constructor {
+		public StringConstructor() {
+			yamlConstructors.put(Tag.TIMESTAMP, new ConstructYamlStr());
+		}
+	}
 
-    /**
-     * Override some of the yaml constructors, so that the value written in the flatten result is more alike with the
-     * source. For instance, timestamps may be written in a completely different format which prevents converters to
-     * convert the correct value.
-     */
-    private static class StringConstructor extends Constructor {
-        public StringConstructor() {
-            this.yamlConstructors.put(Tag.TIMESTAMP, new ConstructYamlStr());
-        }
-    }
+	public String getValueType(String key) {
+		return getValue(TYPE_PREFIX + key);
+	}
 }
--- src/test/java/com/github/wansors/SpringYamlConfigSourceTest.java
+++ src/test/java/com/github/wansors/SpringYamlConfigSourceTest.java
@@ -21,7 +21,11 @@ public class SpringYamlConfigSourceTest {
 
 		Map<String, String> properties = config.getProperties();
 
-		assertEquals(22, properties.size());
+		for (String x : properties.values()) {
+			System.out.println(x);
+		}
+
+		assertEquals(54, properties.size());
 
 		assertEquals("first hash", config.getValue("empty.hash.a"));
 		assertEquals("", config.getValue("empty.hash.b"));
@@ -30,9 +34,11 @@ public class SpringYamlConfigSourceTest {
 		assertEquals("second element", config.getValue("empty.simple_list[1]"));
 		assertEquals("third element", config.getValue("empty.simple_list[2]"));
 		assertEquals("", config.getValue("empty.simple_list[3]"));
+		assertEquals("[]", config.getValue("empty.list"));
 		assertEquals("true", config.getValue("global.var_boolean1"));
 		assertEquals("false", config.getValue("global.var_boolean2"));
 		assertEquals("42", config.getValue("global.var_number"));
+		assertEquals("123123123123123123", config.getValue("global.var_long"));
 		assertEquals("value1", config.getValue("global.var_string"));
 		assertEquals("07760", config.getValue("global.var_string_number"));
 		assertEquals("123", config.getValue("inline.list[0]"));
@@ -45,6 +51,37 @@ public class SpringYamlConfigSourceTest {
 		assertEquals("filter 1", config.getValue("var.complex_list[1].filters[0][0]"));
 		assertEquals("true", config.getValue("var.complex_list[1].filters[0][1]"));
 		assertEquals("node1", config.getValue("var.complex_list[1].node"));
+		assertEquals("[]", config.getValue("string.list"));
+		assertEquals("true", config.getValue("string.boolean"));
+		assertEquals("42", config.getValue("string.number"));
+
+		assertEquals("java.lang.String", config.getValueType("empty.hash.a"));
+		assertEquals("java.lang.String", config.getValueType("empty.hash.b"));
+		assertEquals("java.lang.String", config.getValueType("empty.hash.c"));
+		assertEquals("java.lang.String", config.getValueType("empty.simple_list[0]"));
+		assertEquals("java.lang.String", config.getValueType("empty.simple_list[1]"));
+		assertEquals("java.lang.String", config.getValueType("empty.simple_list[2]"));
+		assertEquals("java.lang.String", config.getValueType("empty.simple_list[3]"));
+		assertEquals("java.util.List", config.getValueType("empty.list"));
+		assertEquals("java.lang.Boolean", config.getValueType("global.var_boolean1"));
+		assertEquals("java.lang.Boolean", config.getValueType("global.var_boolean2"));
+		assertEquals("java.lang.Integer", config.getValueType("global.var_number"));
+		assertEquals("java.lang.Long", config.getValueType("global.var_long"));
+		assertEquals("java.lang.String", config.getValueType("global.var_string"));
+		assertEquals("java.lang.String", config.getValueType("global.var_string_number"));
+		assertEquals("java.lang.Integer", config.getValueType("inline.list[0]"));
+		assertEquals("java.lang.Integer", config.getValueType("inline.list[1]"));
+		assertEquals("java.lang.Integer", config.getValueType("inline.list[2]"));
+		assertEquals("java.lang.Integer", config.getValueType("inline.var"));
+		assertEquals("java.lang.String", config.getValueType("var.complex_list[0].filters[0][0]"));
+		assertEquals("java.lang.Boolean", config.getValueType("var.complex_list[0].filters[0][1]"));
+		assertEquals("java.lang.String", config.getValueType("var.complex_list[0].node"));
+		assertEquals("java.lang.String", config.getValueType("var.complex_list[1].filters[0][0]"));
+		assertEquals("java.lang.Boolean", config.getValueType("var.complex_list[1].filters[0][1]"));
+		assertEquals("java.lang.String", config.getValueType("var.complex_list[1].node"));
+		assertEquals("java.lang.String", config.getValueType("string.list"));
+		assertEquals("java.lang.String", config.getValueType("string.boolean"));
+		assertEquals("java.lang.String", config.getValueType("string.number"));
 
 	}
 }
