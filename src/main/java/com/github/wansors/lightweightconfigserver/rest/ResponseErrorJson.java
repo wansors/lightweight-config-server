bfaefb8384f1619d580d637bec06a113712366c6
wansors <wansors@gmail.com>
2021-05-23 00:14:16 +0200

    minor fixes

--- src/main/java/com/github/wansors/lightweightconfigserver/rest/ResponseErrorJson.java
+++ src/main/java/com/github/wansors/lightweightconfigserver/rest/ResponseErrorJson.java
@@ -2,44 +2,55 @@ package com.github.wansors.lightweightconfigserver.rest;
 
 import java.util.Date;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
 public class ResponseErrorJson {
 
-    private Date timestamp=new Date();
-    private int status=200;
-    private String error;
-    private String message="";
-    private String path;
+	private Date timestamp = new Date();
+	private int status = 200;
+	private String error;
+	private String message = "";
+	private String path;
 
-    public Date getTimestamp() {
-        return timestamp;
-    }
-    public void setTimestamp(Date timestamp) {
-        this.timestamp = timestamp;
-    }
-    public int getStatus() {
-        return status;
-    }
-    public void setStatus(int status) {
-        this.status = status;
-    }
-    public String getError() {
-        return error;
-    }
-    public void setError(String error) {
-        this.error = error;
-    }
-    public String getMessage() {
-        return message;
-    }
-    public void setMessage(String message) {
-        this.message = message;
-    }
-    public String getPath() {
-        return path;
-    }
-    public void setPath(String path) {
-        this.path = path;
-    }
+	public Date getTimestamp() {
+		return timestamp;
+	}
 
-    
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
 }
