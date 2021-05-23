fd63a7937a279882f3f033fe2d9ab82bb40a5669
wansors <wansors@gmail.com>
2021-05-23 14:36:40 +0200

    fixes

--- src/main/java/com/github/wansors/lightweightconfigserver/rest/ApiWsExceptionMapper.java
+++ src/main/java/com/github/wansors/lightweightconfigserver/rest/ApiWsExceptionMapper.java
@@ -1,5 +1,6 @@
 package com.github.wansors.lightweightconfigserver.rest;
 
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
@@ -8,17 +9,25 @@ import javax.ws.rs.ext.Provider;
  * Mapper to deal with exceptions
  */
 @Provider
-public class ApiWsExceptionMapper implements ExceptionMapper<ApiWsException> {
+public class ApiWsExceptionMapper implements ExceptionMapper<Exception> {
 
-    @Override
-    public Response toResponse(ApiWsException exception) {
-        ResponseErrorJson error = new ResponseErrorJson();
+	@Override
+	public Response toResponse(Exception exception) {
+		ResponseErrorJson error = new ResponseErrorJson();
 
-        error.setStatus(exception.getStatusCode());
-        error.setError(exception.getErrorType());
-        error.setMessage(exception.getLocalizedMessage());
+		if (exception instanceof ApiWsException) {
+			error.setStatus(((ApiWsException) exception).getStatusCode());
+			error.setError(((ApiWsException) exception).getErrorType());
+			error.setMessage(exception.getLocalizedMessage());
 
-        return Response.status(exception.getStatusCode()).entity(error).build();
-    }
+		} else {
+			error.setMessage(exception.getMessage());
+			error.setError(ErrorTypeCodeEnum.REQUEST_UNDEFINED_ERROR.getErrorType());
+			error.setStatus(ErrorTypeCodeEnum.REQUEST_UNDEFINED_ERROR.getHttpCode().getCode());
+		}
+
+		return Response.status(error.getStatus()).entity(error).type(MediaType.APPLICATION_JSON).build();
+
+	}
 
 }
