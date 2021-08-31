b154e43646ec656d76bf83d59731d9ddc4a56a00
wansors <wansors@gmail.com>
2021-08-31 11:36:39 +0200

    Fixes on pull with credentials

--- src/main/java/com/github/wansors/lightweightconfigserver/git/GitRepositoryBranch.java
+++ src/main/java/com/github/wansors/lightweightconfigserver/git/GitRepositoryBranch.java
@@ -4,11 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
-import org.eclipse.jgit.api.CloneCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.api.errors.InvalidRemoteException;
-import org.eclipse.jgit.api.errors.TransportException;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import org.jboss.logging.Logger;
 
@@ -17,102 +14,96 @@ import com.github.wansors.lightweightconfigserver.rest.ErrorTypeCodeEnum;
 import com.github.wansors.lightweightconfigserver.utils.FileUtils;
 
 public class GitRepositoryBranch {
-	private static final Logger LOG = Logger.getLogger(GitRepositoryBranch.class);
-	/**
-	 * Time of the last refresh of the git repository.
-	 */
-	private long lastRefresh = 0;
+    private static final Logger LOG = Logger.getLogger(GitRepositoryBranch.class);
+    /**
+     * Time of the last refresh of the git repository.
+     */
+    private long lastRefresh = 0;
 
-	// Internal values
-	private File branchFolder;
+    // Internal values
+    private File branchFolder;
 
-	private GitConfiguration gitConf;
+    private GitConfiguration gitConf;
 
-	public GitRepositoryBranch(GitConfiguration gitConf) {
-		this.gitConf = gitConf;
+    public GitRepositoryBranch(GitConfiguration gitConf) {
+	this.gitConf = gitConf;
+    }
+
+    public GitRepositoryBranch(File branchFolder, GitConfiguration gitConf) {
+	this.branchFolder = branchFolder;
+	this.gitConf = gitConf;
+    }
+
+    public void init() throws IOException, GitAPIException {
+	if (this.branchFolder == null) {
+	    this.branchFolder = Files.createTempDirectory("tmpgit").toFile();
+	    var cloneCommand = Git.cloneRepository();
+	    cloneCommand.setDirectory(this.branchFolder).setCloneAllBranches(true).setURI(this.gitConf.uri());
+	    if (this.gitConf.isAuthenticationEnabled()) {
+		cloneCommand.setCredentialsProvider(new UsernamePasswordCredentialsProvider(this.gitConf.username().orElse(null), this.gitConf.password().orElse(null)));
+	    }
+	    cloneCommand.call();
+
+	    this.lastRefresh = System.currentTimeMillis();
 	}
+    }
 
-	public GitRepositoryBranch(File branchFolder, GitConfiguration gitConf) {
-		this.branchFolder = branchFolder;
-		this.gitConf = gitConf;
-	}
+    protected boolean shouldPull() {
+	return this.gitConf.refreshRate() > 0 && System.currentTimeMillis() - this.lastRefresh < this.gitConf.refreshRate() * 1000;
+    }
 
-	public void init() throws IOException, InvalidRemoteException, TransportException, GitAPIException {
-		if (branchFolder == null) {
-			branchFolder = Files.createTempDirectory("tmpgit").toFile();
-			CloneCommand cloneCommand = Git.cloneRepository();
-			cloneCommand.setDirectory(branchFolder).setCloneAllBranches(true).setURI(gitConf.uri());
-			if (gitConf.isAuthenticationEnabled()) {
-				cloneCommand.setCredentialsProvider(new UsernamePasswordCredentialsProvider(
-						gitConf.username().orElse(null), gitConf.password().orElse(null)));
-			}
-			cloneCommand.call();
+    public File getBranchFolder() {
+	return this.branchFolder;
+    }
 
-			lastRefresh = System.currentTimeMillis();
-		}
-	}
+    public void pull() {
 
-	protected boolean shouldPull() {
-		if (gitConf.refreshRate() > 0 && System.currentTimeMillis() - lastRefresh < (gitConf.refreshRate() * 1000)) {
-			return false;
-		}
-
-		return true;
-	}
-
-	public File getBranchFolder() {
-		return branchFolder;
-	}
-
-	public void pull() {
-
-		if (shouldPull()) {
-			lastRefresh = System.currentTimeMillis();
-			LOG.info("[REPO] Pulling repository " + gitConf.uri());
-			try (Git git = Git.open(branchFolder)) {
-				if (gitConf.isAuthenticationEnabled()) {
-					git.pull().setCredentialsProvider(new UsernamePasswordCredentialsProvider(
-							gitConf.username().orElse(null), gitConf.password().orElse(null)));
-				} else {
-					git.pull().call();
-				}
-
-			} catch (GitAPIException | IOException e) {
-				LOG.warn("pull ", e);
-				throw new ApiWsException(ErrorTypeCodeEnum.REQUEST_UNDEFINED_ERROR, e);
-			}
-
+	if (this.shouldPull()) {
+	    this.lastRefresh = System.currentTimeMillis();
+	    LOG.info("[REPO] Pulling repository " + this.gitConf.uri());
+	    try (var git = Git.open(this.branchFolder)) {
+		if (this.gitConf.isAuthenticationEnabled()) {
+		    git.pull().setCredentialsProvider(new UsernamePasswordCredentialsProvider(this.gitConf.username().orElse(null), this.gitConf.password().orElse(null))).call();
+		} else {
+		    git.pull().call();
 		}
 
-	}
-
-	public Git getGit() throws IOException {
-		return Git.open(branchFolder);
-	}
-
-	public GitRepositoryBranch duplicate(String branchName, String type) {
-		File tmpDestinationDirectory = null;
-		try {
-
-			// Duplicate current dir
-			tmpDestinationDirectory = Files.createTempDirectory("tmpgit").toFile();
-			LOG.debug("Duplicate git repo for " + branchName + " on " + tmpDestinationDirectory.getAbsolutePath());
-			FileUtils.copyDirectory(branchFolder, tmpDestinationDirectory);
-
-			// Change to new branch
-			Git tmpGit = Git.open(tmpDestinationDirectory);
-
-			tmpGit.pull();
-			if (!tmpGit.getRepository().getBranch().equals(branchName)) {
-				LOG.debug("Checking branch " + branchName);
-				tmpGit.checkout().setCreateBranch(true).setName(branchName).setStartPoint(type + branchName).call();
-				lastRefresh = System.currentTimeMillis();
-			}
-		} catch (IOException | GitAPIException e) {
-			LOG.error(e);
-			throw new ApiWsException(ErrorTypeCodeEnum.REQUEST_UNDEFINED_ERROR, e);
-		}
-		return new GitRepositoryBranch(tmpDestinationDirectory, gitConf);
+	    } catch (GitAPIException | IOException e) {
+		LOG.warn("pull ", e);
+		throw new ApiWsException(ErrorTypeCodeEnum.REQUEST_UNDEFINED_ERROR, e);
+	    }
 
 	}
+
+    }
+
+    public Git getGit() throws IOException {
+	return Git.open(this.branchFolder);
+    }
+
+    public GitRepositoryBranch duplicate(String branchName, String type) {
+	File tmpDestinationDirectory = null;
+	try {
+
+	    // Duplicate current dir
+	    tmpDestinationDirectory = Files.createTempDirectory("tmpgit").toFile();
+	    LOG.debug("Duplicate git repo for " + branchName + " on " + tmpDestinationDirectory.getAbsolutePath());
+	    FileUtils.copyDirectory(this.branchFolder, tmpDestinationDirectory);
+
+	    // Change to new branch
+	    var tmpGit = Git.open(tmpDestinationDirectory);
+
+	    tmpGit.pull();
+	    if (!tmpGit.getRepository().getBranch().equals(branchName)) {
+		LOG.debug("Checking branch " + branchName);
+		tmpGit.checkout().setCreateBranch(true).setName(branchName).setStartPoint(type + branchName).call();
+		this.lastRefresh = System.currentTimeMillis();
+	    }
+	} catch (IOException | GitAPIException e) {
+	    LOG.error(e);
+	    throw new ApiWsException(ErrorTypeCodeEnum.REQUEST_UNDEFINED_ERROR, e);
+	}
+	return new GitRepositoryBranch(tmpDestinationDirectory, this.gitConf);
+
+    }
 }
